3aaee11202c1c06e1acf814d61445a3cdd397302
enrique-izel-sns <enrique.izel@supernovacidade.com.br>
2025-10-15 07:00:27 -0400

    feat: transforma imagem perfil para imagem circular
    transforma a imagem de perfil de quadrada para circular por meio de código e não XML

--- app/src/main/java/com/izel/smoothportfolio/MainActivity.kt
+++ app/src/main/java/com/izel/smoothportfolio/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.izel.smoothportfolio
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import com.izel.smoothportfolio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fotoPerfil = BitmapFactory.decodeResource(resources, R.drawable.foto_perfil)
+        val circulo = RoundedBitmapDrawableFactory.create(resources, fotoPerfil)
+        circulo.isCircular = true
+        binding.imgFotoPerfil.setImageDrawable(circulo)
     }
 }
