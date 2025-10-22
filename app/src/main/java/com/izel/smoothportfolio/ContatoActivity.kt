c154c67cc7cbbf8d5fee09a4c69d719752bd6443
enrique-izel-sns <enrique.izel@supernovacidade.com.br>
2025-10-22 07:42:30 -0400

    feat: cria função para abrir o whatsapp
    função para abrir o whatasapp ao clicar no ícone do mesmo

--- app/src/main/java/com/izel/smoothportfolio/ContatoActivity.kt
+++ app/src/main/java/com/izel/smoothportfolio/ContatoActivity.kt
@@ -1,10 +1,10 @@
 package com.izel.smoothportfolio
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.izel.smoothportfolio.databinding.ActivityContatoBinding
 
 class ContatoActivity : AppCompatActivity() {
@@ -18,9 +18,26 @@ class ContatoActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val toolbarContato = binding.toolbarContato
-
         toolbarContato.setNavigationOnClickListener {
             finish()
+        }
+
+        binding.btnWhatsapp.setOnClickListener {
+            abrirWhatsapp()
+        }
+    }
+
+    private fun abrirWhatsapp() {
+        val numeroTelefone = "5592981659117"
+        val uri = Uri.parse("https://api.whatsapp.com/send?phone=$numeroTelefone")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+
+        if (numeroTelefone.equals(numeroTelefone)) {
+            startActivity(intent)
+        } else {
+            val playStoreIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.whatsapp"))
+            startActivity(playStoreIntent)
         }
     }
 }
